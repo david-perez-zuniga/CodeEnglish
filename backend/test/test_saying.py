51136a56ad0267c2a206c51e20986e3f5a90885f
destripador2000 <davidhumbertop503@gmail.com>
2026-05-08 14:32:49 -0600

    Feat: Creación de test éxitoso para ENDPOINT GET de saying, testeo fallido

--- backend/test/test_saying.py
+++ backend/test/test_saying.py
@@ -25,3 +25,26 @@ def test_create_saying_fail(client):
     assert response.status_code == 422
     data = response.json()
     assert "detail" in data
+
+
+# Función test para obtener saying (Éxitoso)
+def test_get_saying_succes(client):
+    id_page_test = 5
+    
+    payload = {
+        "pages_id": id_page_test,
+        "saying": "On the tip of the tongue",
+        "meaning": "En la punta de la lengua",
+        "example": "I had the word on the tip of the tongue"
+    }
+
+    client.post("/api/rt_sayings/create_saying", json=payload)
+    response = client.get(f"/api/rt_sayings/saying/{id_page_test}")
+
+    assert response.status_code == 200
+    data = response.json()
+    assert isinstance(data, list)
+
+    assert len(data) == 1
+    assert data[0]["saying"] == "On the tip of the tongue"
+    assert data[0]["pages_id"] == 5
