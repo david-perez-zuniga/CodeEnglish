41893bf8657db710094f5d846d27c26eee60b2e4
destripador2000 <davidhumbertop503@gmail.com>
2026-05-16 15:54:37 -0600

    Feat: Creación de test éxitoso para ENDPOINT GET de country, testeo éxitoso

--- backend/test/test_country.py
+++ backend/test/test_country.py
@@ -26,3 +26,23 @@ def test_create_country_fail(client):
     assert response.status_code == 422
     data = response.json()
     assert "detail" in data
+
+
+# Test function to get country (Success)
+def test_get_country_succes(client):
+    id_pagina_prueba = 1
+    payload = {
+        "pages_id": id_pagina_prueba,
+        "country": "France",
+        "adjective": "French",
+        "person": "Frenchman"
+    }
+    client.post("/api/rt_countries/create_country", json=payload)
+
+    response = client.get(f"/api/rt_countries/country/{id_pagina_prueba}")
+
+    assert response.status_code == 200
+    data = response.json()
+    assert isinstance(data, list)
+    assert len(data) == 1
+    assert data[0]["country"] == "France"
