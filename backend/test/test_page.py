72f299d1d0a0e472446dede235192cc2b114c22a
destripador2000 <davidhumbertop503@gmail.com>
2026-05-16 16:47:58 -0600

    Feat: Creación de test fallido para ENDPOINT POST de page, testeo éxitoso

--- backend/test/test_page.py
+++ backend/test/test_page.py
@@ -14,3 +14,14 @@ def test_create_page_succes(client):
     assert data["page_number"] == 1
     assert data["module_type"] == "vocabulary"
     assert "id" in data
+
+
+# Test function to create a page (Fail)
+def test_create_page_fail(client):
+    payload_incomplete = {"page_number": 1}
+
+    response = client.post("/api/rt_pages/create_page", json=payload_incomplete)
+
+    assert response.status_code == 422
+    data = response.json()
+    assert "detail" in data
